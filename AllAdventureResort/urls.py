d4043b3eac0ec6326e1d74d719ac4fd72c270eb3
manish gautam <200104@softwarica.edu.np>
2022-01-24 22:02:03 +0545

    view users and admins

--- AllAdventureResort/urls.py
+++ AllAdventureResort/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
-    path('',include('accounts.urls')),
-    path('resort/',include('resort.urls')),
-    path('admins/', include('admins.urls'))
+    path('', include('accounts.urls')),
+    path('resort/', include('resort.urls')),
+    path('admins/', include('admins.urls')),
+    path('admin/', admin.site.urls)
 
 ]
--- resort/urls.py
+++ resort/urls.py
@@ -1,4 +1,24 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('home', views.home),]
+    path('home', views.home),
+    path('activity_type_form', views.activity_type_form),
+    path('get_activity_type', views.get_activity_type),
+    path('delete_activity_type/<int:activity_type_id>', views.delete_activity_type),
+    path('update_activity_type/<int:activity_type_id>', views.update_activity_type),
+
+    path('activity_form', views.activity_form),
+    path('get_activity', views.get_activity),
+    path('delete_activity/<int:activity_id>', views.delete_activity),
+    path('update_activity/<int:activity_id>', views.update_activity),
+    path('activities_view', views.view_activities),
+
+    path('delete_cart_activity/<int:cart_id>', views.delete_cart_activity),
+    path('add_to_cart/<int:activity_id>', views.add_to_cart),
+    path('user_cart', views.view_cart_activities),
+
+    path('order_form/<int:activity_id>/<int:cart_id>', views.order_form),
+    path('user_order', views.user_order),
+    path('esewa_verify', views.esewa_verify),
+
+]
